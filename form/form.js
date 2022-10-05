4f601c2eec96c75f12632e087048639c499c3f8a
krvtc <krabtc@bk.ru>
2022-10-05 21:42:05 +0300

    added new function

--- form/form.js
+++ form/form.js
@@ -12,9 +12,27 @@ submit_button.addEventListener('click', ()=>{
         object[key] = value;
     });
     let json = JSON.stringify(object,undefined,2);
-    form_view.textContent = json
+    form_view.innerHTML = '<pre>' + json + '</pre>';
+    dataRequest(formData);
 });
 
+
+const host_query = '/server/'; /*абстрактный сервер*/
+
+async function dataRequest(formData){
+    const data = [...formData.entries()];// получаем сущности из объекта формы
+    const queryString = data //перебираем массив сущностей и создаем из него строку запроса
+        .map(x => `${encodeURIComponent(x[0])}=${encodeURIComponent(x[1])}`)
+        .join('&');
+
+    const response = await fetch(`${host_query}?${queryString}`);
+    if(response.ok){  /*проверка на успешность получения данных сервером*/
+        alert("Success")
+        return;
+    }
+    alert('Error')
+    return;
+}
 
 
 
